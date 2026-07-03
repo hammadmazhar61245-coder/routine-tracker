ac6efd0df7d20f7c3cb44d166f209580f7643aa7
Hammadmazhar61245 <hammadmazhar512@gmail.com>
2026-07-03 16:51:49 +0500

    stop tracking .next build output

--- .next/dev/static/chunks/app_page_tsx_19f-8j9._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_16vj1tz._.js","static/chunks/node_modules_framer-motion_dist_es_0pq85ks._.js","static/chunks/node_modules_motion-dom_dist_es_1e_8ir3._.js","static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_0q79z23._.js","static/chunks/node_modules_dexie_09juad-._.js","static/chunks/node_modules_0cg9jb2._.js"],
-    source: "dynamic"
-});
